abf1a7c5630c5d69d768d75d2e6c0a9ef6578550
aravi201 <aravindhsura55@gmail.com>
2026-08-14 13:00:05 +0530

    fibonacci sereis using loops

--- src/com/logicalstatements/loops/Fibonacci.java
+++ src/com/logicalstatements/loops/Fibonacci.java
@@ -6,7 +6,7 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("eneter the number");
+		System.out.println("enter the number");
 		int n=sc.nextInt();
 		
 		int first=0;
