bfb38f51721d7be6e69c5a2ec7547701ff1cff56
aravi201 <aravindhsura55@gmail.com>
2026-08-25 18:03:46 +0530

    Copying array from one array to another array

---
+++ src/com/arrays/CopyArray.java
@@ -0,0 +1,23 @@
+package com.arrays;
+
+public class CopyArray {
+
+	public static void main(String[] args) {
+		int[] arr1= {3,4,5,6,7,8};
+		int [] arr2=new int[arr1.length];
+		
+		for(int i=0;i<arr1.length;i++) {
+			arr2[i]=arr1[i];
+		}
+		
+		System.out.println("the original array is");
+		for(int i=0;i<arr1.length;i++) 
+		System.out.print(arr1[i]+ " ");
+		System.out.println();
+		System.out.println("the copied array is");
+		for(int i=0;i<arr1.length;i++) 
+		System.out.print(arr2[i]+ " ");
+		
+	}
+
+}
